06b9e7b4aeeebb622eaa74ebc4c358f499e790cd
DevMatheusEd <matheus.carneiro@escolar.ifrn.edu.br>
2026-05-24 14:57:34 -0300

    wip:Adicionado métodos e configurações
    métodos post, get, put e delete. inicio de uma configuração do JWT

--- Controllers/AlunosController.cs
+++ Controllers/AlunosController.cs
@@ -13,5 +13,74 @@ public class AlunosController : ControllerBase
 
     public AlunosController(EscolaDbContext db) => _db = db;
 
-    
+    [HttpGet]
+    public async Task<IActionResult> GetAlunos()
+    {
+        var alunos = await _db.Alunos.ToListAsync();
+        return Ok(alunos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAluno(int id)
+    {
+        var aluno = await _db.Alunos.FindAsync(id);
+        if (aluno == null)
+        {
+            return NotFound();
+        }
+        return Ok(aluno);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAluno(Aluno aluno)
+    {
+        _db.Alunos.Add(aluno);
+        await _db.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetAluno), new { id = aluno.Id }, aluno);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAluno(int id, Aluno aluno)
+    {
+        if (id != aluno.Id)
+        {
+            return BadRequest();
+        }
+
+        _db.Entry(aluno).State = EntityState.Modified;
+
+        try
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!AlunoExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAluno(int id)
+    {
+        var aluno = await _db.Alunos.FindAsync(id);
+        if (aluno == null)
+        {
+            return NotFound();
+        }
+
+        _db.Alunos.Remove(aluno);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -13,6 +13,29 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+
+var jwtKey = builder.Configuration["Jwt:Key"];
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtKey)
+            )
+        };
+    });
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
